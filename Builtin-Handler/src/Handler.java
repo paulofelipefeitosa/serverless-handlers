cfdb77321c3171bd16d3c7263c34ef7258a3a02a
paulofelipefeitosa <paulofelipe.feitosa@gmail.com>
2018-11-16 17:52:09 -0300

    Get container startup time

--- Builtin-Handler/src/Handler.java
+++ Builtin-Handler/src/Handler.java
@@ -1,4 +1,6 @@
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.management.ManagementFactory;
@@ -7,6 +9,7 @@ public class Handler {
     public static void main(String[] args) throws IOException {
         long readyTime = System.currentTimeMillis();
         long jvmStartTime = ManagementFactory.getRuntimeMXBean().getStartTime();
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String line = null;
         StringBuilder builder = new StringBuilder();
@@ -15,10 +18,21 @@ public class Handler {
             builder.append((line != null) ? line + System.lineSeparator() : "");
         } while (line != null);
         long readyToProcessTime = System.currentTimeMillis();
+        br.close();
+        
+        // Read docker run timestamps
+        File file = new File("timestamps.ts");
+        br = new BufferedReader(new FileReader(file));
+        line = null;
+        do {
+            line = br.readLine();
+            builder.append((line != null) ? line + System.lineSeparator() : "");
+        } while (line != null);
+        
         builder.append("JVMStartTime: " + jvmStartTime + System.lineSeparator());
         builder.append("ReadyTime: " + readyTime + System.lineSeparator());
         builder.append("ReadyToProcessTime: " + readyToProcessTime + System.lineSeparator());
-        System.out.println(builder.toString());
         br.close();
+        System.out.println(builder.toString());
     }
 }
