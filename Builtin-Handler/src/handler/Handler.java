079320bf8ec80db9ca0ca8cd432353f0ff5d865a
paulofelipefeitosa <paulofelipe.feitosa@gmail.com>
2018-11-16 14:14:23 -0300

    Add dockerfile

--- Builtin-Handler/src/handler/Handler.java
+++ Builtin-Handler/src/handler/Handler.java
@@ -1,11 +1,11 @@
-package handlers;
+package handler;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.management.ManagementFactory;
 
-public class BuiltInHandler {
+public class Handler {
     public static void main(String[] args) throws IOException {
         long jvmStartTime = ManagementFactory.getRuntimeMXBean().getStartTime();
         long readyTime = System.currentTimeMillis();
