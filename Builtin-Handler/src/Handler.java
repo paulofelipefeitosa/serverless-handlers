cf85fb11feafe9c9bf9352e2188f986d46a833f0
paulofelipefeitosa <paulofelipe.feitosa@gmail.com>
2018-11-20 14:57:27 -0300

    Simplifies buffered read in stdin

--- Builtin-Handler/src/Handler.java
+++ Builtin-Handler/src/Handler.java
@@ -4,31 +4,33 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.management.ManagementFactory;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Handler {
     public static void main(String[] args) throws IOException {
         long readyTime = System.currentTimeMillis();
         long jvmStartTime = ManagementFactory.getRuntimeMXBean().getStartTime();
-        
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String line = null;
+
         StringBuilder builder = new StringBuilder();
-        do {
-            line = br.readLine();
-            builder.append((line != null) ? line + System.lineSeparator() : "");
-        } while (line != null);
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+                Stream<String> stream = in.lines()) {
+            builder.append(stream.collect(Collectors.joining(System.lineSeparator())));
+            builder.append(System.lineSeparator());
+        }
         long readyToProcessTime = System.currentTimeMillis();
-        br.close();
-        
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         // Read docker run timestamps
+
         File file = new File("timestamps.ts");
         br = new BufferedReader(new FileReader(file));
-        line = null;
+        String line = null;
         do {
             line = br.readLine();
             builder.append((line != null) ? line + System.lineSeparator() : "");
         } while (line != null);
-        
+
         builder.append("JVMStartTime: " + jvmStartTime + System.lineSeparator());
         builder.append("ReadyTime: " + readyTime + System.lineSeparator());
         builder.append("ReadyToProcessTime: " + readyToProcessTime + System.lineSeparator());
