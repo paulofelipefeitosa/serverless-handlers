ed4a343b1a4fb62f5341ad8f7515d47eeed2921b
paulofelipefeitosa <paulofelipe.feitosa@gmail.com>
2019-02-12 11:27:00 -0300

    Add std server handler

---
+++ std-server-handler/execute-requests.go
@@ -0,0 +1,75 @@
+package main
+
+import(
+	"os"
+	"os/exec"
+	"log"
+	"time"
+	"strconv"
+	"fmt"
+	"io"
+	"io/ioutil"
+)
+
+func main() {
+	nRequests, err := strconv.ParseInt(os.Args[1], 10, 64)
+	jarPath := os.Args[2]
+	executionID := os.Args[3]
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for i := int64(0); i < nRequests; i++ {
+		upServerCmd := exec.Command("java", "-jar", jarPath)
+		upServerCmd.Env = os.Environ()
+		
+		stderrPipe, stdinPipe, stdoutPipe, err := getPipes(upServerCmd)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		startHandlerTS := time.Now().UTC().UnixNano()
+		if err := upServerCmd.Start(); err != nil {
+			log.Fatal(err)
+		}
+		// escrever no stdin
+		var startHandlerServiceTS, endHandlerServiceTS int64
+		fmt.Fscanf(stderrPipe, "T5: %d", &startHandlerServiceTS)
+		fmt.Fscanf(stderrPipe, "T6: %d", &endHandlerServiceTS)
+		endHandlerTS := time.Now().UTC().UnixNano()
+		
+		startHandlerServiceTS *= 1e6
+		endHandlerServiceTS *= 1e6
+
+		io.Copy(ioutil.Discard, stdoutPipe)
+		stderrPipe.Close()
+		stdinPipe.Close()
+		stdoutPipe.Close()
+		
+		upServerCmd.Process.Wait()
+
+		fmt.Printf("%s,%s,%d,%d\n", "RuntimeReadyTime", executionID, i, startHandlerServiceTS - startHandlerTS)
+		fmt.Printf("%s,%s,%d,%d\n", "ServiceTime", executionID, i, endHandlerServiceTS - startHandlerServiceTS)
+		fmt.Printf("%s,%s,%d,%d\n", "RoundTripTime", executionID, i, endHandlerTS - startHandlerTS)
+
+		time.Sleep(5 * time.Millisecond)
+	}
+
+}
+
+func getPipes(command* exec.Cmd) (io.ReadCloser, io.WriteCloser, io.ReadCloser, error) {
+	stderrPipe, err := command.StderrPipe()
+	if err != nil {
+		return stderrPipe, nil, nil, err 
+	}
+	stdinPipe, err := command.StdinPipe()
+	if err != nil {
+		return stderrPipe, stdinPipe, nil, err 
+	}
+	stdoutPipe, err := command.StdoutPipe()
+	if err != nil {
+		return stderrPipe, stdinPipe, stdoutPipe, err 
+	}
+	return stderrPipe, stdinPipe, stdoutPipe, nil
+}
