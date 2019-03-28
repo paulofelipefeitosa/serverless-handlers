cbe8d0e87da783c14cf4af0f34af786c8d5bbf39
paulofelipefeitosa <paulofelipe.feitosa@gmail.com>
2019-03-28 17:43:38 -0300

    Try to get current timestamp on bpftrace

--- jvmti-agent/run-test.sh
+++ jvmti-agent/run-test.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 OUTPUT_FILENAME=$1
 ABS_PATH=`pwd`
-echo "strace -fff -ttt -T -e trace=fork,clone,execve,mmap,openat"
+#echo "strace -fff -ttt -T -e trace=fork,clone,execve,mmap,openat"
 if [ -z "$OUTPUT_FILENAME" ]
 then
-    echo "["$(date +%s%N)"] Run Command" && java -agentpath:$ABS_PATH/libagent.so -classpath . Nothing
+    #bpftrace -c "java -agentpath:${ABS_PATH}/libagent.so -classpath . Nothing" read.bt
+    java -agentpath:${ABS_PATH}/libagent.so -classpath . Nothing
 else
-    echo "["$(date +%s%N)"] Run Command" > $OUTPUT_FILENAME && java -agentpath:$ABS_PATH/libagent.so="$OUTPUT_FILENAME" -classpath . Nothing
+    truncate --size=0 $OUTPUT_FILENAME
+    #bpftrace -c "java -agentpath:${ABS_PATH}/libagent.so=${OUTPUT_FILENAME} -classpath . Nothing" read.bt
+    java -agentpath:${ABS_PATH}/libagent.so=${OUTPUT_FILENAME} -classpath . Nothing >> $OUTPUT_FILENAME
 fi
