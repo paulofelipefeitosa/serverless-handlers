8d9d6a601b29a31c062110360d068e667ead1c0c
paulofelipefeitosa <paulofelipe.feitosa@gmail.com>
2019-03-29 10:30:48 -0300

    Removing unnecessary code

--- jvmti-agent/run-test.sh
+++ jvmti-agent/run-test.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 OUTPUT_FILENAME=$1
 ABS_PATH=`pwd`
-#echo "strace -fff -ttt -T -e trace=fork,clone,execve,mmap,openat"
 if [ -z "$OUTPUT_FILENAME" ]
 then
-    #bpftrace -c "java -agentpath:${ABS_PATH}/libagent.so -classpath . Nothing" read.bt
     java -agentpath:${ABS_PATH}/libagent.so -classpath . Nothing
 else
-    truncate --size=0 $OUTPUT_FILENAME
-    #bpftrace -c "java -agentpath:${ABS_PATH}/libagent.so=${OUTPUT_FILENAME} -classpath . Nothing" read.bt
     java -agentpath:${ABS_PATH}/libagent.so=${OUTPUT_FILENAME} -classpath . Nothing >> $OUTPUT_FILENAME
 fi
