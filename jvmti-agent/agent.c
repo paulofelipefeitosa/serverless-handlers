b70eeeae68128fcc7ca7201db7d0a90dac6be487
paulofelipefeitosa <paulofelipe.feitosa@gmail.com>
2019-04-01 17:37:11 -0300

    Remove unnecessary events

--- jvmti-agent/agent.c
+++ jvmti-agent/agent.c
@@ -8,8 +8,9 @@ static void check_jvmti_errors(jvmtiEnv *jvmti, jvmtiError errnum, const char *s
 static void trace(jvmtiEnv *jvmti_env, const char* fmt, ...);
 
 static jvmtiEnv *jvmti;
-static jrawMonitorID rawMonitorID;
+static jrawMonitorID vmtrace_lock;
 static struct timespec tms;
+static FILE* out;
 
 static long long getCurrentTimestamp() {
     if(clock_gettime(CLOCK_MONOTONIC, &tms)) {
@@ -21,9 +22,10 @@ static long long getCurrentTimestamp() {
 }
 
 static void trace(jvmtiEnv *jvmti_env, const char* fmt, ...) {
-    (*jvmti_env)->RawMonitorEnter(jvmti_env, rawMonitorID);
+    (*jvmti_env)->RawMonitorEnter(jvmti_env, vmtrace_lock);
 
-    long long current_time = getCurrentTimestamp();
+    jlong current_time;
+    (*jvmti_env)->GetTime(jvmti_env, &current_time);
 
     char buf[1024];
     va_list args;
@@ -31,9 +33,9 @@ static void trace(jvmtiEnv *jvmti_env, const char* fmt, ...) {
     vsprintf(buf, fmt, args);
     va_end(args);
 
-    printf("[%lld] %s\n", current_time, buf);
+    fprintf(out, "[%lld] %s\n", current_time, buf);
 
-    (*jvmti_env)->RawMonitorExit(jvmti_env, rawMonitorID);
+    (*jvmti_env)->RawMonitorExit(jvmti_env, vmtrace_lock);
 }
 
 static char* fix_class_name(char* class_name) {
@@ -55,57 +57,6 @@ void JNICALL VMInit(jvmtiEnv *jvmti_env,
 void JNICALL VMDeath(jvmtiEnv *jvmti_env, 
         JNIEnv* jni_env) {
     trace(jvmti_env, "VM destroyed");
-}
-
-/* Load class data file */
-void JNICALL ClassFileLoadHook(jvmtiEnv *jvmti_env,
-            JNIEnv* jni_env,
-            jclass class_being_redefined,
-            jobject loader,
-            const char* name,
-            jobject protection_domain,
-            jint class_data_len,
-            const unsigned char* class_data,
-            jint* new_class_data_len,
-            unsigned char** new_class_data) {
-    trace(jvmti_env, "Loading class: %s", name);
-}
-
-/* When class is loaded (fields, methods and implemented interfaces) but no class code is executed yet */
-void JNICALL ClassPrepare(jvmtiEnv *jvmti_env,
-            JNIEnv* jni_env,
-            jthread thread,
-            jclass klass) {
-    char* name;
-    (*jvmti_env)->GetClassSignature(jvmti_env, klass, &name, NULL);
-    trace(jvmti_env, "Class prepared: %s", fix_class_name(name));
-    (*jvmti_env)->Deallocate(jvmti_env, name);
-}
-
-/* Compiling JVM internal code */
-void JNICALL DynamicCodeGenerated(jvmtiEnv *jvmti_env,
-            const char* name,
-            const void* address,
-            jint length) {
-    trace(jvmti_env, "Dynamic code generated: %s", name);
-}
-
-void JNICALL CompiledMethodLoad(jvmtiEnv *jvmti_env,
-            jmethodID method,
-            jint code_size,
-            const void* code_addr,
-            jint map_length,
-            const jvmtiAddrLocationMap* map,
-            const void* compile_info) {
-    jclass holder;
-    char* holder_name;
-    char* method_name;
-    (*jvmti_env)->GetMethodName(jvmti_env, method, &method_name, NULL, NULL);
-    (*jvmti_env)->GetMethodDeclaringClass(jvmti_env, method, &holder);
-    (*jvmti_env)->GetClassSignature(jvmti_env, holder, &holder_name, NULL);
-    trace(jvmti_env, "Method compiled: %s.%s", fix_class_name(holder_name), method_name);
-    (*jvmti_env)->Deallocate(jvmti_env, method_name);
-    (*jvmti_env)->Deallocate(jvmti_env, holder_name);
 }
 
 static void setEventNotification(jvmtiEnv* env,
@@ -167,7 +118,14 @@ JNIEXPORT jint JNICALL Agent_OnLoad(JavaVM *jvm,
             void *reserved) {
     long long start_time = getCurrentTimestamp();
 
-    printf("[%lld] JVMTI started\n", start_time);
+    if (options == NULL || !options[0]) {
+        out = stdout;
+    } else if ((out = fopen(options, "w")) == NULL) {
+        fprintf(stderr, "Cannot open output file: %s\n", options);
+        return 1;
+    }
+
+    fprintf(out, "[%lld] JVMTI started\n", start_time);
 
     jvmtiError error;
     jint res;
@@ -177,15 +135,13 @@ JNIEXPORT jint JNICALL Agent_OnLoad(JavaVM *jvm,
     // Get the JVMTI environment
     res = (*jvm)->GetEnv(jvm, (void **) &jvmti, JVMTI_VERSION_1_0);
     if (res != JNI_OK || jvmti == NULL) {
-        printf("Unable to get access to JVMTI version 1.0");
+        fprintf(stderr, "Unable to get access to JVMTI version 1.0");
     }
-    trace(jvmti, "VMTrace started");
+    //trace(jvmti, "VMTrace started");
     
     (void) memset(&capa, 0, sizeof(jvmtiCapabilities));
 
     // Let's initialize the capabilities
-    capa.can_generate_all_class_hook_events = 1;
-    capa.can_generate_compiled_method_load_events = 1;
     capa.can_generate_method_entry_events = 1;
     capa.can_generate_method_exit_events = 1;
     error = (*jvmti)->AddCapabilities(jvmti, &capa);
@@ -195,10 +151,6 @@ JNIEXPORT jint JNICALL Agent_OnLoad(JavaVM *jvm,
     setEventNotification(jvmti, JVMTI_ENABLE, JVMTI_EVENT_VM_START);
     setEventNotification(jvmti, JVMTI_ENABLE, JVMTI_EVENT_VM_INIT);
     setEventNotification(jvmti, JVMTI_ENABLE, JVMTI_EVENT_VM_DEATH);
-    setEventNotification(jvmti, JVMTI_ENABLE, JVMTI_EVENT_CLASS_FILE_LOAD_HOOK);
-    setEventNotification(jvmti, JVMTI_ENABLE, JVMTI_EVENT_CLASS_PREPARE);
-    setEventNotification(jvmti, JVMTI_ENABLE, JVMTI_EVENT_DYNAMIC_CODE_GENERATED);
-    setEventNotification(jvmti, JVMTI_ENABLE, JVMTI_EVENT_COMPILED_METHOD_LOAD);
     setEventNotification(jvmti, JVMTI_ENABLE, JVMTI_EVENT_METHOD_ENTRY);
     setEventNotification(jvmti, JVMTI_ENABLE, JVMTI_EVENT_METHOD_EXIT);
 
@@ -207,17 +159,13 @@ JNIEXPORT jint JNICALL Agent_OnLoad(JavaVM *jvm,
     callbacks.VMStart = VMStart;
     callbacks.VMInit = VMInit;
     callbacks.VMDeath = VMDeath;
-    callbacks.ClassFileLoadHook = ClassFileLoadHook;
-    callbacks.ClassPrepare = ClassPrepare;
-    callbacks.DynamicCodeGenerated = DynamicCodeGenerated;
-    callbacks.CompiledMethodLoad = CompiledMethodLoad;
     callbacks.MethodEntry = MethodEntry;
     callbacks.MethodExit = MethodExit;
     error = (*jvmti)->SetEventCallbacks(jvmti, &callbacks, (jint) sizeof(callbacks));
     check_jvmti_errors(jvmti, error, "Unable to set event callbacks");
 
     // Get the raw monitor
-    error = (*jvmti)->CreateRawMonitor(jvmti, "JVMTI agent data", &rawMonitorID);
+    error = (*jvmti)->CreateRawMonitor(jvmti, "JVMTI agent data", &vmtrace_lock);
     check_jvmti_errors(jvmti, error, "Unable to create a Raw monitor");
 
     return JNI_OK;
@@ -230,6 +178,12 @@ static void check_jvmti_errors(jvmtiEnv *jvmti,
         char *errnum_str;
         errnum_str = NULL;
         (void) (*jvmti)->GetErrorName(jvmti, errnum, &errnum_str);
-        printf("ERROR: JVMTI: [%d] %s - %s\n", errnum, (errnum_str == NULL ? "Unknown": errnum_str), (str == NULL? "" : str));
+        fprintf(stderr, "ERROR: JVMTI: [%d] %s - %s\n", errnum, (errnum_str == NULL ? "Unknown": errnum_str), (str == NULL? "" : str));
+    }
+}
+
+JNIEXPORT void JNICALL Agent_OnUnload(JavaVM *vm) {
+    if (out != NULL && out != stderr) {
+        fclose(out);
     }
 }
