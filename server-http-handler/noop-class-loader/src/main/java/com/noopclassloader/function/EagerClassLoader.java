64559bc0152398566c993aa9bb8f6e1981b7b6de
paulofelipefeitosa <paulofelipe.feitosa@gmail.com>
2019-06-06 13:27:24 -0300

    Remove streams to vanish overhead

--- server-http-handler/noop-class-loader/src/main/java/com/noopclassloader/function/EagerClassLoader.java
+++ server-http-handler/noop-class-loader/src/main/java/com/noopclassloader/function/EagerClassLoader.java
@@ -1,104 +1,58 @@
 package com.noopclassloader.function;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Enumeration;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
 public class EagerClassLoader extends ClassLoader {
 
     private JarFile jarFile;
-    private final int bufferSize = 5000;
-    byte[] buffer = new byte[this.bufferSize];
-    private Map<String, Class<?>> mapClass;
-    private Map<String, LoadStats> stats;
+    private long rt = 0L;
+    private long ct = 0L;
+    private int counter = 0;
 
     public EagerClassLoader(String jarFilePath) throws IOException {
         this.jarFile = new JarFile(jarFilePath);
-        this.mapClass = new HashMap<String, Class<?>>();
-        this.stats = new HashMap<String, LoadStats>();
     }
 
     public String loadJarClasses() throws IOException {
         long startTime = System.nanoTime();
         try {
-            this.jarFile
-                .stream()
-                .filter(e -> (!e.isDirectory()
-                            && e.getName().startsWith("generated")
-                            && e.getName().endsWith(".class")))
-                .allMatch(e -> {
+            ClassLoader cLoader = getClass().getClassLoader();
+            Enumeration<JarEntry> entries = this.jarFile.entries();
+            while (entries.hasMoreElements()) {
+                JarEntry e = entries.nextElement();
+                if (!e.isDirectory() && e.getName().startsWith("generated")
+                        && e.getName().endsWith(".class")) {
+
+                    String className = e.getName().replaceAll("\\.class", "").replaceAll("/", ".");
+                    try {
+                        long sr = System.nanoTime();
+                        Class<?> c = cLoader.loadClass(className);
+                        long si = System.nanoTime();
                         try {
-                            String className =
-                                    e.getName().replaceAll("\\.class", "").replaceAll("/", ".");
-                            loadClass(className, false);
-                            return true;
-                        } catch (ClassNotFoundException e1) {
-                            e1.printStackTrace();
-                            return true;
+                            c.getConstructor().newInstance();
+                        } catch (Throwable ee) {
+                            ee.printStackTrace();
                         }
-                });
-            return "Total Load Time: " + (System.nanoTime() - startTime) + System.lineSeparator()
-                    + this.stats.values().stream()
-                            .reduce(new LoadStats(0L, 0L, 0L), (a, e) -> a.add(e)).toString()
-                    + System.lineSeparator() + "Loaded Classes: " + this.stats.size();
+                        long end = System.nanoTime();
+
+                        this.rt += si - sr;
+                        this.ct += end - si;
+                        this.counter++;
+                    } catch (ClassNotFoundException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+            }
+            return "Loading Classes Time: " + this.rt + System.lineSeparator()
+                    + "Interpreting Classes Time: " + this.ct + System.lineSeparator()
+                    + "Loaded Classes: " + this.counter + System.lineSeparator()
+                    + "Total Load Time: " + (System.nanoTime() - startTime);
         } finally {
             this.jarFile.close();
         }
-    }
-
-    @Override
-    protected Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
-        Class<?> c = this.mapClass.get(name);
-        if (c != null) {
-            return c;
-        }
-        JarEntry entry = this.jarFile.getJarEntry(name.replaceAll("\\.", "/") + ".class");
-        if (entry != null) {
-            try (InputStream is = this.jarFile.getInputStream(entry)) {
-                long sr = System.nanoTime();
-                byte[] byteArr = toByteArray(is);
-                c = defineClass(name, byteArr, 0, byteArr.length);
-                
-                long si = System.nanoTime(), end = 0L;
-                try {
-                    if (name.startsWith("generated")) {
-                        c.newInstance();
-                        end = System.nanoTime();
-                    } else {
-                        end = System.nanoTime();
-                    }
-                } catch (Throwable e) {
-                    e.printStackTrace();
-                }
-
-                this.stats.put(name, new LoadStats(si - sr, end - si, byteArr.length));
-                
-                this.mapClass.put(name, c);
-                return c;
-            } catch (IOException e) {
-                throw new ClassNotFoundException(e.toString());
-            }
-        } else {
-            long sr = System.nanoTime();
-            c = getClass().getClassLoader().loadClass(name);
-            long end = System.nanoTime();
-            this.stats.put(name, new LoadStats(0, end - sr, 0));
-            return c;
-        }
-    }
-
-    private byte[] toByteArray(InputStream inputStream) throws IOException {
-        ByteArrayOutputStream out = new ByteArrayOutputStream(this.bufferSize);
-        int read;
-        while ((read = inputStream.read(buffer, 0, buffer.length)) != -1) {
-            out.write(buffer, 0, read);
-        }
-        out.flush();
-        return out.toByteArray();
     }
 
 }
