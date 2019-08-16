4d9e345717acc7d7bb054528390226ebb6076e80
paulofelipefeitosa <paulofelipe.feitosa@gmail.com>
2019-08-16 19:46:27 -0300

    Add thumbnailator nodejs v8 implementation

--- server-http-handler/nodejs/thumbnailator/handler.js
+++ server-http-handler/nodejs/thumbnailator/handler.js
@@ -3,38 +3,30 @@ const scale = parseFloat(process.env.scale)
 const image_path = process.env.image_path
 const image = sharp(image_path)
 
-function resizer(size, image) {
-    try {
-		size.then((size) => {
-			image
-			.resize(size.width, size.height)
-			.toBuffer()
-			//.toFile('output.jpg')
-			//.then(data => {})	
+function resizer(scale, image) {
+	return image
+		.metadata()
+		.then((metadata) => {
+			return image
+				.resize(metadata.width * scale, metadata.height * scale)
+				.toBuffer()
+				//.toFile('output.jpg')
+				//.then(data => {})	
 		})
-    } catch (error) {
-    	throw new Error(error)
-    }
 }
 
 function handle(getTimestamp, req, res) {
 	console.log('T4: ' + getTimestamp())
-	const new_size = image
-		.metadata()
-		.then((metadata) => {
-			return {width: metadata.width * scale, height: metadata.height * scale}
-		})
-	resizer(new_size, image)
-		.then(() => {
+	resizer(scale, image)
+		.then((info) => {
 			res.writeHead(200);
-			console.log('T6: ' + getTimestamp())
-			return res
 		})
 		.catch((error) => {
 			res.writeHead(500);
 			res.write(error)
-			return res
 		})
+	console.log('T6: ' + getTimestamp())
+	return res
 }
 
 module.exports.handle = handle;
