fb6c3636821a2765a3b96955478b52fa53350262
paulofelipefeitosa <paulofelipe.feitosa@gmail.com>
2019-08-16 17:08:01 -0300

    Add NOOP handler

--- server-http-handler/nodejs/noop/handler.js
+++ server-http-handler/nodejs/noop/handler.js
@@ -1,20 +1,8 @@
-const process = require('process')
-console.log('EIM: ' + getNanoRealTime())
-
-function getNanoRealTime() {
-	timestamps = process.hrtime()
-	return (timestamps[0] * 1e9) + timestamps[1]
+module.exports = {
+	handle: function(getTimestamp, req, res) {
+		console.log('T4: ' + getTimestamp())
+		res.writeHead(200);
+		console.log('T6: ' + getTimestamp())
+		return res
+	}
 }
-
-const http = require('http')
-const port = 9000
-const ip = 'localhost'
-
-const server = http.createServer((req, res) => {
-	console.log('T4: ' + getNanoRealTime())
-	res.writeHead(200);
-	console.log('T6: ' + getNanoRealTime())
-	res.end()
-}).listen(port, ip)
-
-console.log('EFM: ' + getNanoRealTime())
