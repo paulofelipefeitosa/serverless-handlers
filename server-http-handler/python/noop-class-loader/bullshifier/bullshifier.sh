1b48432670fa173d9c1d1e8683786e1b60e09166
paulofelipefeitosa <paulofelipe.feitosa@gmail.com>
2019-09-17 17:57:27 -0300

    Add bullshifier implementation

--- server-http-handler/python/noop-class-loader/bullshifier/bullshifier.sh
+++ server-http-handler/python/noop-class-loader/bullshifier/bullshifier.sh
@@ -7,15 +7,15 @@ do
 		BASE_CODE_PATH="${i#*=}"
 		shift 
 		;;
-		-c=*|--copies=*) # Number of base code copies 
+		-cps=*|--copies=*) # Number of base code copies 
 		COPIES="${i#*=}"
 		shift
 		;;
-		-p=*|--preffix=*) # String preffix that will be added before the pattern replacement
+		-pre=*|--preffix=*) # String preffix that will be added before the pattern replacement
 		PREFFIX="${i#*=}"
 		shift
 		;;
-		-s=*|--suffix=*) # String suffix that will be added after the pattern replacement
+		-suf=*|--suffix=*) # String suffix that will be added after the pattern replacement
 		SUFFIX="${i#*=}"
 		shift
 		;;
@@ -32,5 +32,7 @@ FILE_EXTENSION=$(echo $BASE_CODE_PATH | awk -F "." '{print $2}')
 
 for i in $(seq "$COPIES")
 do
-	cp $BASE_CODE_PATH $PATTERN$i$FILE_EXTENSION
+	new_filename=$PATTERN$i.$FILE_EXTENSION
+	cp $BASE_CODE_PATH $new_filename
+	sed -i "s/$PREFFIX$PATTERN$SUFFIX/$PREFFIX$PATTERN$i$SUFFIX/" $new_filename
 done
