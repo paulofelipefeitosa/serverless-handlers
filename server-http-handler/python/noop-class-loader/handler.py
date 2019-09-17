e9726b17d236a00c3319797254ccadd0260d1071
paulofelipefeitosa <paulofelipe.feitosa@gmail.com>
2019-09-17 17:57:52 -0300

    Add read app config in noop class loader handler

--- server-http-handler/python/noop-class-loader/handler.py
+++ server-http-handler/python/noop-class-loader/handler.py
@@ -1,4 +1,27 @@
 from utils.http_entities import Response
+import sys
 
-def handle(request):
-	return Response()
+class Handler:
+
+	def __init__(self):
+		self.config = self.read_config()
+
+	def read_config(self):
+		try:
+			app_config_path = sys.argv[-1]
+			f = open(app_config_path, 'r')
+			fn_ptt = "base_filename="
+			n_classes = "classes="
+			for line in f:
+				fn_ptt_arr = line.split(fn_ptt)
+				n_classes_arr = line.split(n_classes)
+				if len(fn_ptt_arr) > 1:
+					filename = fn_ptt_arr[1]
+				if len(n_classes_arr) > 1:
+					classes = n_classes_arr[1]
+			return (filename, classes)
+		finally:
+			f.close()
+
+	def handle(self, request):
+		return Response()
