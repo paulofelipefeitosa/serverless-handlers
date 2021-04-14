90ad8481adfdf125ba490115707a1474ef518356
Paulo Felipe <paulofelipe.feitosa@gmail.com>
2021-04-14 11:32:41 -0300

    Add multiple executions tool

--- seuss/exec.sh
+++ seuss/exec.sh
@@ -8,18 +8,25 @@ echo "Executing experiment with the following parameters: ($ROUNDS, 1, 1, 1)"
 
 echo "$ROUNDS, 1, 1, 1" | docker attach mycontainer &
 
-sleep 1
+sleep 3
 
 lines=$((2 * "$ROUNDS"))
+max_retries=100
+count=0
 
 while
   docker logs mycontainer &> "$LOG_FILEPATH"
   # shellcheck disable=SC2126
-  len=$(grep -A1 "Scheduling activation" < "$LOG_FILEPATH" | wc -l)
-  if [ "$len" == "$lines" ];
+  len=$(grep -A1 "Scheduling activation tid" < "$LOG_FILEPATH" | wc -l)
+  if [ "$len" -ge "$lines" ];
   then
     break
+  elif [ "$count" -ge "$max_retries" ];
+  then
+    echo "Max retries reached, exiting with status 10"
+    exit 10
   else
+    count=$((count + 1))
     sleep 1
   fi
 do
